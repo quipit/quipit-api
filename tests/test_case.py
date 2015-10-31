b039c517750e373a91bcfca8d0bc6504b1a984a4
Jonathan Como <jonathan.como@gmail.com>
2015-10-31 15:50:30 -0700

    Fix tests

--- tests/test_case.py
+++ tests/test_case.py
@@ -1,14 +1,18 @@
 from unittest import TestCase
 
-from quipit.app import db
+from quipit.app import app
+from quipit.db import db
 
 
 class DBTestCase(TestCase):
     def setUp(self):
         super(DBTestCase, self).setUp()
+        self.ctx = app.app_context()
+        self.ctx.push()
         db.create_all()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        self.ctx.pop()
         super(DBTestCase, self).tearDown()
