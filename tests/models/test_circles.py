6718b2cb12274f3392d67089f6e791b9282122ca
Jonathan Como <jonathan.como@gmail.com>
2015-11-01 12:04:31 -0800

    Clean up circle testsg

--- tests/models/test_circles.py
+++ tests/models/test_circles.py
@@ -5,81 +5,43 @@ from tests.test_case import DBTestCase
 
 
 class CircleTestCase(DBTestCase):
-    def test_it_can_add_members_without_saving(self):
-        circle = Circle('SF Crew')
-
-        user = User('Jonathan Como', 'jcomo')
-        other_user = User('Peter Como', 'pcomo')
-
-        circle.add_member(user)
-        self.assertEqual(1, len(circle.members.all()))
-
-        circle.add_member(other_user)
-        self.assertEqual(2, len(circle.members.all()))
-
-        self.assertIsNone(circle.id)
+    def setUp(self):
+        super(CircleTestCase, self).setUp()
+        self.user = User('Jonathan Como', 'jcomo')
+        self.circle = Circle('SF Crew')
+        self.circle.add_member(self.user)
 
     def test_it_does_not_add_duplicate_members(self):
-        circle = Circle('SF Crew')
-        user = User('Jonathan Como', 'jcomo')
+        self.circle.add_member(self.user)
 
-        circle.add_member(user)
-        circle.add_member(user)
-
-        self.assertEqual(1, len(circle.members.all()))
+        self.assertEqual(1, self.circle.member_count)
 
     def test_it_can_have_many_members(self):
-        circle = Circle('SF Crew')
-
-        user = User('Jonathan Como', 'jcomo')
         other_user = User('Peter Como', 'pcomo')
 
-        circle.add_member(user)
-        circle.add_member(other_user)
+        self.circle.add_member(other_user)
 
-        db.session.add(circle)
+        db.session.add(self.circle)
         db.session.commit()
 
-        self.assertEqual(sorted([user, other_user]), sorted(circle.members.all()))
+        self.assertEqual(sorted([self.user, other_user]), sorted(self.circle.members.all()))
+
+    def test_it_does_not_add_duplicate_quips(self):
+        quip = Quip('Some circle thangs?', self.user)
+
+        self.circle.add_quip(quip)
+        self.circle.add_quip(quip)
+
+        self.assertEqual(1, self.circle.quip_count)
 
     def test_it_can_have_many_quips(self):
-        circle = Circle('SF Crew')
+        quip = Quip('Some circle thangs?', self.user)
+        other_quip = Quip('Some other thangs', self.user)
 
-        user = User('Jonathan Como', 'jcomo')
-        circle.add_member(user)
+        self.circle.add_quip(quip)
+        self.circle.add_quip(other_quip)
 
-        quip = Quip('Some circle thangs?', user)
-        other_quip = Quip('Some other thangs', user)
-
-        circle.add_quip(quip)
-        circle.add_quip(other_quip)
-
-        db.session.add(circle)
+        db.session.add(self.circle)
         db.session.commit()
 
-        self.assertEqual(sorted([quip, other_quip]), sorted(circle.quips.all()))
-
-    def test_it_reports_number_of_quips(self):
-        circle = Circle('SF Crew')
-
-        user = User('Jonathan Como', 'jcomo')
-        circle.add_member(user)
-
-        quip = Quip('Some circle thangs?', user)
-        other_quip = Quip('Some other thangs', user)
-
-        circle.add_quip(quip)
-        circle.add_quip(other_quip)
-
-        self.assertEqual(2, circle.quip_count)
-
-    def test_it_reports_number_of_members(self):
-        circle = Circle('SF Crew')
-
-        user = User('Jonathan Como', 'jcomo')
-        other_user = User('Peter Como', 'pcomo')
-
-        circle.add_member(user)
-        circle.add_member(other_user)
-
-        self.assertEqual(2, circle.member_count)
+        self.assertEqual(sorted([quip, other_quip]), sorted(self.circle.quips.all()))
